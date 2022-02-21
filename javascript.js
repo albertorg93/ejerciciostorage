446e6684ac81641eb31bec614059a62bc6e4dd21
albertorg93 <futberto1993@gmail.com>
2022-02-21 15:41:11 +0100

    añadiendo usuario al DOM

--- javascript.js
+++ javascript.js
@@ -8,16 +8,12 @@ let formulario = document.querySelector("form[name='formulario'");
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log("holaaadklscm");
+   
        let fname = event.target.fname.value;
        let femail = event.target.femail.value;
        let fimage = event.target.fimage.value;
        let ftext = event.target.ftext.value;
 
-      console.log(fname);
-      console.log(femail);
-      console.log(fimage);
-      console.log(ftext);
       localStorage.setItem("name", fname); // op1
       localStorage.setItem("email", femail); // op1
       localStorage.setItem("image", fimage); // op1
@@ -27,6 +23,17 @@ formulario.addEventListener('submit', (event) => {
     let firstEmail = localStorage.getItem("email");
     let firstImage = localStorage.getItem("image");
     let firstText = localStorage.getItem("text");
+
+
+      const mostrarDatos = function(){
+          let section = document.querySelector("#section");
+          section.innerHTML=`<h3>${firstName}</h3>
+          <h3>${firstEmail}</h3>
+          <h3>${firstImage}</h3>
+          <h3>${firstText}</h3>`
+      }
+         
+   mostrarDatos();
 
 })
  
